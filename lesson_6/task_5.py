60c204b9cfd2fd28f62072f26de26704fdef7a31
kotikov <kotikov@isands.ru>
2022-09-26 17:24:32 +0700

    Added answers for 5-6 tasks

--- lesson_6/task_5.py
+++ lesson_6/task_5.py
@@ -1,22 +1,15 @@
 '''5- Найти произведение пар чисел в списке. Парой считаем первый и последний элемент, второй и предпоследний и т.д.'''
 
+from tkinter import N
+
+
 digits = [123, 12, 65, 84, 25, 43, 86, 35, 90]
 
-result = digits[:2:]
+first_part = digits[:int(len(digits)/2):]
+second_part = list(reversed(digits[int(len(digits)/2)::]))
+
+result = list(map(lambda x, y: x + y, first_part, second_part))
+if (len(digits) % 2 != 0): 
+    result.append(second_part[-1])
 
 print(result)
-
-# for i in range(int(len(digits)/2)):
-    # result.append(digits[i] + digits[len(digits)-1 - i])
-# if (len(digits) % 2 != 0):
-    # result.append(digits[int(len(digits)/2)])
-# print (result)
-
-
-
-
-
-# print(list(filter(lambda char: char.isdigit(), ['1', '2', 'f', '4'])))
-
-
-# print(reduce(lambda element_1, element_2: element_1+element_2, [ord(char) for char in 'python']))
