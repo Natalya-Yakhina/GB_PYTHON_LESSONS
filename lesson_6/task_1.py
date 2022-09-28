1a6ec852f7b1fd85d2fa603ecd01fc6b627b14f2
Natalya-Yakhina <107551668+Natalya-Yakhina@users.noreply.github.com>
2022-09-28 14:00:58 +0300

    Delete lesson_6 directory

--- lesson_6/task_1.py
+++
@@ -1,8 +0,0 @@
-'''1- Определить, присутствует ли в заданном списке строк, некоторое число'''
-
-strings = ['123', 'cake', 'joo', '34', 'foo', 'Main', '89']
-search_str = '34'
-
-if len(list(filter(lambda x: x == search_str, strings))) > 0:
-    print(True)
-else: print(False)
--- lesson_6/task_6.py
+++
@@ -1,9 +0,0 @@
-'''6-Сформировать список из N членов последовательности.
-Для N = 5: 1, -3, 9, -27, 81 и т.д'''
-
-n = 5
-lst = []
-
-list(map(lambda x: lst.append(1 if x == 0 else lst[-1] * -3), [y for y in range(n)]))
-
-print(lst)
